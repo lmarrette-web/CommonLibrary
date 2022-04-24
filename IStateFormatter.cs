7c758d1cbe7976a04bcb391d0e1776b5d43f0de0
lmarrette-web <54044831+lmarrette-web@users.noreply.github.com>
2022-04-24 13:33:46 -0500

    Add state formatter

---
+++ IStateFormatter.cs
@@ -0,0 +1,16 @@
+﻿// Created By:           Lee Marrette
+//           Date:           //
+//     Solution:           JobVite
+//       Project:           CommonLibrary
+namespace CommonLibrary
+{
+    public interface IStateFormatter
+    {
+        /// <summary>
+        /// Returns abbreviation for state 
+        /// </summary>
+        /// <param name="inState">Name of state</param>
+        /// <returns>Abbreviated name</returns>
+        string ReturnStateAbbreviation(string inState);
+    }
+}
